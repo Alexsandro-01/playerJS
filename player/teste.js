409e9a9acc7275efe5e44a4be352885ec3a04c18
Alexsandro Pinheiro Ferreira <73038442+Alexsandro-01@users.noreply.github.com>
2021-06-25 12:40:33 -0300

    Add files via upload

---
+++ player/teste.js
@@ -0,0 +1,26 @@
+/*
+let i = 0
+let interval
+
+function s() {
+    console.log('S')
+    interval = setInterval(timer, 1000 )
+    
+}
+
+function stop() {
+    //console.log('Stop')
+    clearInterval(interval)
+
+}
+
+function timer() {
+    i++
+    console.log(i)
+
+}
+*/
+
+let a = 255.84725 % 60
+
+console.log(a)
